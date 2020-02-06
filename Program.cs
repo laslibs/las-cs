d899e3571ae9994c16bd7d8e5a766740abc35379
Onyekwere Kelechi Mark <31822485+Khelechy@users.noreply.github.com>
2020-02-06 16:13:25 +0100

    initial LasCs methods and config (#1)
    no error report handlers, all errors are consoled out to the terminal, all methods are exposed.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using las_cs.src;
+using System;
+
+namespace las_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string path = @"C:\Users\khelechy\Documents\GitHub\las-cs\test.las";
+			string test = "#ignore me\nDon't ignore me\n#   \nEHllo # don't ignore";
+			LasCs LasEngine = new LasCs(path);
+			string result = LasEngine.other();
+			//Console.WriteLine(LasCs.removeComment(path));
+
+			//foreach (string s in result)
+			//{
+			//	Console.WriteLine(s);
+			//}
+			Console.WriteLine(result);
+
+		}
+    }
+}
